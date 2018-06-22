c15fe611c699e78247f67ff442067503202be704
KarinaMealey <kem32002@gmail.com>
2018-06-22 16:01:40 +0000

    automatically backed up by learn

--- table.rb
+++ table.rb
@@ -1,3 +1,39 @@
 class Some_assembly_required
   
+  def initialize(language, height, sibling_age)
+      @language = language
+      @height = height
+      @sibling_age = sibling_age
+    end 
+    
+ 
+  def basic_facts 
+    puts "have brown hair"
+    puts "in high school"
+    puts "like bok choy"
+    puts "like bananas"
+  end 
+  
+  def what_language
+    @language
+  end 
+  
+  def what_height
+    @height
+  end 
+  
+  def age_of_sibling
+    @sibling_age
+  end 
+  end 
+  
+  puts "Give me some basic facts #{karina.basic_facts}"
+  
+  karina = Some_assembly_required.new("english", "5ft 6in", "12 years old")
+  puts "What language do you speak? #{karina.what_language}"
+  puts "How tall are you? #{karina.what_height}"
+  puts "How old is your sibling #{karina.age_of_sibling}"
+  
+  
+  
   
