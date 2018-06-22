9bec90e78896837214c4adc746508110b30039fe
KarinaMealey <kem32002@gmail.com>
2018-06-22 15:21:26 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -16,4 +16,4 @@ tiger = Cats.new("very","pretty", "super")
 puts "Is the tiger cute? #{tiger.is_it_cute}"
 
 garfield = Cats.new("no, he has an attitude", "up to debate", "yes")
-puts "Is Garfield cute? #{garfield.is_it_cute}"
+# puts "Is Garfield cute? #{garfield.is_it_cute}"
